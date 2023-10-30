4697f9e5ebd1be7bac4bef8cdab005141027bc24
Quang <nghiemquang2002@gmail.com>
2023-10-30 21:23:33 +0700

    add Week4

---
+++ Week4/README.md
@@ -0,0 +1,25 @@
+# OS COURSE: HW WEEK 4
+> by: **Van Quang Nghiem**
+## Usage
+* To run the test code, enter the following command in the terminal:
+First, complie main.c to main.exe
+```shell
+// gcc -pthread -o main main.c
+```
+after that, execute main.exe file:
+```shell
+./main
+```
+## Running Tests
+Results after running:
+```shell
+(base) quang@quang-HP-ProBook-430-G6:~/Desktop/OS_course/Week4$ ./main
+[21:17:23] [INFO]: Parent thread
+Enter the value of x: 5
+Enter the value of y: 7
+[21:17:26] [INFO]: Child thread
+[21:17:26] [INFO]: Finished Calculation
+[21:17:26] [INFO]: Parent thread
+The value of z is 12
+
+```
---
+++ Week4/main.c
@@ -0,0 +1,71 @@
+// gcc -pthread -o main main.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+
+// Define a structure for shared var
+struct Var {
+    int x, y, z;
+    volatile int ready; // Use volatile to prevent compiler optimization
+};
+
+// Function to perform calculation
+void calculate(struct Var* var) {
+    var->z = var->x + var->y;
+    var->ready = 0;
+}
+
+// Function to log messages
+void logMessage(const char* tag, const char* message) {
+    time_t rawtime;
+    struct tm* timeinfo;
+    char timestamp[20]; // Buffer to store formatted timestamp
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    strftime(timestamp, sizeof(timestamp), "%H:%M:%S", timeinfo);
+    printf("[%s] [%s]: %s\n", timestamp, tag, message);
+}
+
+void* childThread(void* arg) {
+    struct Var* var = (struct Var*)arg;
+    
+    // Wait until ready flag is set by the parent
+    while (var->ready == 1) {
+        sleep(1); // Sleep for 1 second to reduce CPU usage
+    }
+
+    // Perform the calculation
+    logMessage("INFO", "Child thread");
+    calculate(var);
+    logMessage("INFO", "Finished Calculation");
+
+    pthread_exit(NULL);
+}
+
+int main() {
+    struct Var var;
+    var.ready = 1; // Set ready flag to 1 initially
+    
+    pthread_t thread;
+    pthread_create(&thread, NULL, childThread, (void*)&var);
+
+    logMessage("INFO", "Parent thread");
+    printf("Enter the value of x: ");
+    scanf("%d", &var.x);
+    printf("Enter the value of y: ");
+    scanf("%d", &var.y);
+
+    // Notify child to start calculation
+    var.ready = 0;
+
+    // Wait for the child thread to finish
+    pthread_join(thread, NULL);
+
+    logMessage("INFO", "Parent thread");
+    // Display the calculated result
+    printf("The value of z is %d\n", var.z);
+
+    return 0;
+}
