188e86c7e6aa0fa468bf28bd202b8ac2377082e9
Nghiêm Văn Quang <nghiemquang2002@gmail.com>
2023-11-01 23:19:43 +0700

    update SJF

--- Week4_1/main_SJF.cpp
+++ Week4_1/main_SJF.cpp
@@ -12,7 +12,7 @@ struct Process {
     int burst_time;
     int current_time = 0;  // Completion time
     int tunr_artime = 0; // Turnaround time
-    int waiting_time = 0;  // Waiting time
+    int wait_pooling_time = 0;  // wait_pooling time
 };
 
 struct CmpBurstTime {
@@ -28,9 +28,9 @@ void SJF_Scheduling(const vector<Process>& processes) {
     });
 
     int current_time = 0;
-    priority_queue<Process, vector<Process>, CmpBurstTime> wait;
+    priority_queue<Process, vector<Process>, CmpBurstTime> wait_pool;
     int totalTurnaroundTime = 0;
-    int totalWaitingTime = 0;
+    int totalwait_poolingTime = 0;
 
     ofstream outputFile("output.txt");
     if (outputFile.is_open()) {
@@ -45,26 +45,26 @@ void SJF_Scheduling(const vector<Process>& processes) {
 
         cout << "\nProcess scheduling according to SJF is:" << endl;
 
-        cout << "Process id\tArrival time\tBurst time\tCompletion time\tTurnaround time\tWaiting time" << endl;
+        cout << "Process id\tArrival time\tBurst time\tCompletion time\tTurnaround time\twait_pooling time" << endl;
         outputFile << "\nProcess scheduling according to SJF is:" << endl;
-        outputFile << "Process id\tArrival time\tBurst time\tCompletion time\tTurnaround time\tWaiting time" << endl;
-        while (!inputProcesses.empty() || !wait.empty()) {
+        outputFile << "Process id\tArrival time\tBurst time\tCompletion time\tTurnaround time\twait_pooling time" << endl;
+        while (!inputProcesses.empty() || !wait_pool.empty()) {
             while (!inputProcesses.empty() && inputProcesses[0].arrival_time <= current_time) {
-                wait.push(inputProcesses[0]);
+                wait_pool.push(inputProcesses[0]);
                 inputProcesses.erase(inputProcesses.begin());
             }
 
-            if (!wait.empty()) {
-                auto process = wait.top();
-                wait.pop();
+            if (!wait_pool.empty()) {
+                auto process = wait_pool.top();
+                wait_pool.pop();
                 process.current_time = current_time + process.burst_time;
                 process.tunr_artime = process.current_time - process.arrival_time;
-                process.waiting_time = process.tunr_artime - process.burst_time;
+                process.wait_pooling_time = process.tunr_artime - process.burst_time;
 
-                cout << "\t" << process.process_name << "\t\t" << process.arrival_time << "\t\t" << process.burst_time << "\t\t" << process.current_time << "\t\t" << process.tunr_artime << "\t\t" << process.waiting_time << endl;
-                outputFile << "\t" << process.process_name << "\t\t" << process.arrival_time << "\t\t" << process.burst_time << "\t\t" << process.current_time << "\t\t" << process.tunr_artime << "\t\t" << process.waiting_time << endl;
+                cout << "\t" << process.process_name << "\t\t" << process.arrival_time << "\t\t" << process.burst_time << "\t\t" << process.current_time << "\t\t" << process.tunr_artime << "\t\t" << process.wait_pooling_time << endl;
+                outputFile << "\t" << process.process_name << "\t\t" << process.arrival_time << "\t\t" << process.burst_time << "\t\t" << process.current_time << "\t\t" << process.tunr_artime << "\t\t" << process.wait_pooling_time << endl;
                 totalTurnaroundTime += process.tunr_artime;
-                totalWaitingTime += process.waiting_time;
+                totalwait_poolingTime += process.wait_pooling_time;
 
                 current_time = process.current_time;
             } else if (!inputProcesses.empty()) {
@@ -73,11 +73,11 @@ void SJF_Scheduling(const vector<Process>& processes) {
         }
 
         double averageTurnaroundTime = static_cast<double>(totalTurnaroundTime) / processes.size();
-        double averageWaitingTime = static_cast<double>(totalWaitingTime) / processes.size();
+        double averagewait_poolingTime = static_cast<double>(totalwait_poolingTime) / processes.size();
         cout << "\nAverage Turnaround Time: " << averageTurnaroundTime << endl;
-        cout << "Average Waiting Time: " << averageWaitingTime << endl;
+        cout << "Average wait_pooling Time: " << averagewait_poolingTime << endl;
         outputFile << "\nAverage Turnaround Time: " << averageTurnaroundTime << endl;
-        outputFile << "Average Waiting Time: " << averageWaitingTime << endl;
+        outputFile << "Average wait_pooling Time: " << averagewait_poolingTime << endl;
         outputFile.close();
         cout << "Output written to output.txt" << endl;
     } else {
