49e5d5386482f9f95bfbea27566f448199918659
Quang <nghiemquang2002@gmail.com>
2023-11-22 00:06:22 +0700

    update

--- Week6/main.cpp
+++ Week6/main.cpp
@@ -2,7 +2,7 @@
  * @author Nghiem Van Quang
  * '''g++ main.cpp -o main
     ./main'''
- * @source https://github.com/highquanglity/OS_course/tree/main/Week6
+ * @github https://github.com/highquanglity/OS_course/tree/main/Week6
  */
 #include <iostream>
 #include <vector>
